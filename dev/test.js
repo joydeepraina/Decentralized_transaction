6442e99ed175390b66644546b7b4f83d453adbbc
joydeep singh <65455625+joydeepraina@users.noreply.github.com>
2021-07-05 07:45:28 +0530

    Add files via upload

---
+++ dev/test.js
@@ -0,0 +1,28 @@
+const Blockchain=require('./blockchain');
+
+const bitcoin =new Blockchain();
+
+// const  previousBlockHash='KOJIA122345NJ23RESV3';
+// const currentBlockData=[
+//     {
+//         amount: 10,
+//         sender:'NJSFB21942249BJAD',
+//         recipient:'NKJHIU2899SYBSDC'
+//     },
+//     {
+//         amount: 30,
+//         sender:'NJEFB21942249BJADB',
+//         recipient:'NKRRRRU2899SYBSDC'
+//     }, {
+//         amount: 200,
+//         sender:'NQQQQFB21942249BJADB',
+//         recipient:'GHRDGHIU2899SYBSDC'
+//     }
+// ];
+
+// // const nonce=100;
+
+
+// console.log(bitcoin.hashBlock(previousBlockHash,currentBlockData,1557));
+
+console.log(bitcoin);
